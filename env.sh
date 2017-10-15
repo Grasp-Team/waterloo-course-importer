ad5e46d572a18c892eaf7c2654f266b3e5b39d7b
Jitin Dodd <j2dodd@uwaterloo.ca>
2017-10-15 17:54:37 -0400

    refactored code

---
+++ env.sh
@@ -0,0 +1,12 @@
+# fill this in with the vars on prod
+# then run:
+# chmod +x env.sh
+# source ./env.sh
+
+export API_ENDPOINT='https://api.uwaterloo.ca/v2'
+export API_KEY=
+export DB_HOST=
+export DB_NAME=
+export DB_PASS=
+export DB_USER=
+export DB_PORT=
