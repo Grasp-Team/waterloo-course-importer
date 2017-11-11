321096d4c8250b52f2c3cb93461157f5d1fee856
Jitin Dodd <j2dodd@uwaterloo.ca>
2017-11-11 00:48:21 -0500

    bug fixes on importer

--- importer.rb
+++ importer.rb
@@ -26,9 +26,9 @@ class CourseImporter
   def create_prepared_insert(conn, table_name)
     conn.prepare(
       'insert_course',
-      "insert into #{table_name} (id, code, subject, catalog_number,
+      "insert into #{table_name} (code, subject, catalog_number,
        course_name, description, academic_level, calendar_year, url)
-       values ($1, $2, $3, $4, $5, $6, $7, $8, $9)")
+       values ($1, $2, $3, $4, $5, $6, $7, $8)")
   end
 
   def courses
@@ -44,11 +44,11 @@ class CourseImporter
     puts "#{courses.count} courses retrieved."
     puts "Getting detailed information for each course:"
     data = []
-    courses.each do |course, index|
+    courses.each_with_index do |course, index|
       course['course_code'] = "#{course['subject']}#{course['catalog_number']}"
       puts 'Getting data for: ' + course['course_code']
       #per course, make another request to get detailed info:
-      uri = get_uri('/courses/' + courses[0]['course_id'] + '.json')
+      uri = get_uri('/courses/' + courses[index]['course_id'] + '.json')
       course_info = JSON.parse(Net::HTTP.get(uri))['data']
       course['description'] = course_info['description']
       course['calendar_year'] = course_info['calendar_year']
@@ -60,16 +60,22 @@ class CourseImporter
   end
 
   def write_to_db(conn, courses)
-    puts "Writing #{courses.count} courses to the db."
-    courses.each do |course|
-      conn.exec_prepared(
-        'insert_course',
-        [
-         course['course_id'], course['course_code'], course['subject'],
-         course['catalog_number'], course['title'], course['description'],
-         course['academic_level'], course['calendar_year'], course['url']
-        ]
-      )
+    puts "\nWriting #{courses.count} courses to the db."
+    courses.each_with_index do |course|
+      begin
+        puts "Inserting: #{course['course_code']}"
+        conn.exec_prepared(
+          'insert_course',
+          [
+           course['course_code'], course['subject'],
+           course['catalog_number'], course['title'], course['description'],
+           course['academic_level'], course['calendar_year'], course['url']
+          ]
+        )
+        puts "\tSuccess"
+      rescue Exception
+        puts "\tFail"
+      end
     end
   end
 end
