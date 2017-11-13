335ff8b26760756c0aff111ac7c188a7fc14bd16
Jitin Dodd <j2dodd@uwaterloo.ca>
2017-11-12 23:08:37 -0500

    added subject insert

--- importer.rb
+++ importer.rb
@@ -7,7 +7,7 @@ class CourseImporter
     puts 'Running importer...'
     conn = get_db_connection
     data = get_course_specific_data(courses)
-    create_prepared_insert(conn, "course.course_catalog")
+    create_prepared_insert(conn)
     write_to_db(conn, data)
   end
 
@@ -23,12 +23,15 @@ class CourseImporter
     )
   end
 
-  def create_prepared_insert(conn, table_name)
+  def create_prepared_insert(conn)
     conn.prepare(
-      'insert_course',
-      "insert into #{table_name} (code, subject, catalog_number,
+      'insert_course_catalog',
+      "insert into course.course_catalog (code, subject, catalog_number,
        course_name, description, academic_level, calendar_year, url)
        values ($1, $2, $3, $4, $5, $6, $7, $8)")
+    conn.prepare(
+      'insert_subject',
+      "insert into course.subjects (subject) values ($1)")
   end
 
   def courses
@@ -45,32 +48,56 @@ class CourseImporter
     puts "Getting detailed information for each course:"
     data = []
     courses.each_with_index do |course, index|
-      course['course_code'] = "#{course['subject']}#{course['catalog_number']}"
-      puts 'Getting data for: ' + course['course_code']
-      #per course, make another request to get detailed info:
-      uri = get_uri('/courses/' + courses[index]['course_id'] + '.json')
-      course_info = JSON.parse(Net::HTTP.get(uri))['data']
-      course['description'] = course_info['description']
-      course['calendar_year'] = course_info['calendar_year']
-      course['academic_level'] = course_info['academic_level']
-      course['url'] = course_info['url']
-      data.push(course)
+      begin
+        course['course_code'] = "#{course['subject']}#{course['catalog_number']}"
+        puts 'Getting data for: ' + course['course_code']
+        #per course, make another request to get detailed info:
+        uri = get_uri('/courses/' + courses[index]['course_id'] + '.json')
+        course_info = JSON.parse(Net::HTTP.get(uri))['data']
+        course['description'] = course_info['description']
+        course['calendar_year'] = course_info['calendar_year']
+        course['academic_level'] = course_info['academic_level']
+        course['url'] = course_info['url']
+        data.push(course)
+        puts "\tData retrieved."
+      rescue
+        puts "\tRetrieval failed."
+      end
     end
     data
   end
 
   def write_to_db(conn, courses)
     puts "\nWriting #{courses.count} courses to the db."
+    subjects = Set.new
     courses.each_with_index do |course|
       begin
+        subjects.add(course['subject'])
         puts "Inserting: #{course['course_code']}"
         conn.exec_prepared(
-          'insert_course',
+          'insert_course_catalog',
           [
            course['course_code'], course['subject'],
            course['catalog_number'], course['title'], course['description'],
            course['academic_level'], course['calendar_year'], course['url']
           ]
+        )
+        puts "\tSuccess"
+      rescue Exception
+        puts "\tFail"
+      end
+    end
+    write_subjects(conn, subjects)
+  end
+
+  def write_subjects(conn, subjects)
+    puts "\nWriting #{subjects.count} subjects to the db."
+    subjects.each_with_index do |subject|
+      begin
+        puts "Inserting: #{subject}"
+        conn.exec_prepared(
+          'insert_subject',
+          [subject]
         )
         puts "\tSuccess"
       rescue Exception
